e480176aa5c6fe50ef811cdef1e0d376bae12e6c
notyourbeast <saishiragave@gmail.com>
2025-04-20 07:10:40 +0530

    Add latest updates and uploaded files

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'womens-empowerment',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'womens-empowerment',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
